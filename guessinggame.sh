9a71220b7a529d15ba6e416a620232c0ffc1800a
shivanandfutane <49516633+shivanandfutane@users.noreply.github.com>
2019-04-12 14:55:06 +0530

    Update guessinggame.sh
    Modified the review comments

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,28 +1,21 @@
-echo "[The Guessing game...]"
 
-	echo "You have to tell us the number of files present inside the directory:"
-	read -p "number of files" guess
-    files=$(ls -1 | wc -l)
+#!/usr/bin/env bash
 
 
-
-
-while [ $guess -ne $files ]
-do
-	if [ $guess -lt $files ] 
-	then
-		echo "Too low."
-		
-		exit
-		
-	else
-	
-		echo "Too high."
-	
-	exit
-	
-	fi
-	
-done
-echo "Well done! It is the correct answer, here is the list of files:"
-echo "---" && ls -1
+    true_ans=$(ls -l |grep "^-"|wc -l)
+    while true;
+    do
+	echo "You have to tell us the number of files present inside the directory:"        
+	echo "please enter your guess"
+        read  number
+        if [ $number -lt $true_ans ]
+        then
+            echo "Too low: Your guess is Less than the true number"
+        elif [ $number -gt $true_ans ]
+        then
+            echo "Too high: Your guess is Greater than the true number"
+        else
+            echo " congratulations, Well done! you are right!"
+        break;
+        fi
+    done
